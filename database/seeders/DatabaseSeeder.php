0b5f4f2d1d571f4b8d351b1143770beaa14175c9
bjornwann <bjorn.wann@chasacademy.se>
2021-01-31 23:33:28 +0100

    change seeder to use only books

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         //\App\Models\User::factory(10)->create();
-        $this->call(AuthorTableSeeder::class);
+        //$this->call(AuthorTableSeeder::class);
         $this->call(BookTableSeeder::class);
         
     }
